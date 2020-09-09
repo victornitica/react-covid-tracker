9355b8be5b56f1a1844250b9c59c60f3a03c05a3
victornitica <victornitica@gmail.com>
2020-09-10 02:18:37 +0300

    Updates

---
+++ precache-manifest.e75909ef3451c0e4f382bb775346d52c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b08549639b421622d1577fde7afc6ea0",
+    "url": "/react-covid-tracker/index.html"
+  },
+  {
+    "revision": "a35514bcb5c9531657af",
+    "url": "/react-covid-tracker/static/css/main.4d16253b.chunk.css"
+  },
+  {
+    "revision": "aa180f295cf324bbaa10",
+    "url": "/react-covid-tracker/static/js/2.4ed6d601.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/react-covid-tracker/static/js/2.4ed6d601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a35514bcb5c9531657af",
+    "url": "/react-covid-tracker/static/js/main.0def5edf.chunk.js"
+  },
+  {
+    "revision": "602e351f01d974f3301f",
+    "url": "/react-covid-tracker/static/js/runtime-main.be705594.js"
+  }
+]);
